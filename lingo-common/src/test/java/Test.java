d4ff16ad1ce28f1eb545f0a16d6228a5ce4757d6
Moncoder <ljy1021030020@outlook.com>
2024-03-20 13:34:01 +0800

    配置设置

--- lingo-common/src/test/java/Test.java
+++ lingo-common/src/test/java/Test.java
@@ -1,6 +1,8 @@
 import com.moncoder.lingo.common.api.Result;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 
 /**
  * @author Moncoder
@@ -8,34 +10,8 @@ import java.util.Arrays;
  * @description TODO
  * @date 2024/3/9 18:14
  */
-import java.util.*;
-
-class Main {
-
+class Test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] a = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = sc.nextInt();
-            }
-        }
-        int[][] dp = new int[n][n];
-        dp[0][0] = a[0][0];
-        for(int i = 1; i < n; i++){
-            dp[i][0] = dp[i-1][0] + a[i][0];
-        }
-        for(int j = 1; j < n; j++){
-            dp[0][j] = dp[0][j - 1] + a[0][j];
-        }
-        for (int i = 1; i < n; i++){
-            for(int j = 1; j < n; j++){
-                dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]) + a[i][j];
-            }
-        }
-        System.out.println(dp[n-1][n-1]);
+
     }
-
-
 }

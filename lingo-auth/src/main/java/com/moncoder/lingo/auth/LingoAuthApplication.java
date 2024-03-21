5bb0cc05d046363105538849c832c8bac74e5cb2
Moncoder <ljy1021030020@outlook.com>
2024-03-21 15:19:44 +0800

    统一认证授权中心配置

---
+++ lingo-auth/src/main/java/com/moncoder/lingo/auth/LingoAuthApplication.java
@@ -0,0 +1,13 @@
+package com.moncoder.lingo.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingoAuthApplication.class, args);
+    }
+
+}
---
+++ lingo-auth/src/test/java/com/moncoder/lingo/auth/LingoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moncoder.lingo.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

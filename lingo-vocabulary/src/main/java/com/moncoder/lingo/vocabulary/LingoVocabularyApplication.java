e62540df60f23f71c2b41ccafc0c1fd1f31fe1e6
Moncoder <ljy200220192020@outlook.com>
2024-06-11 13:19:26 +0800

    共享配置及服务监控实现

--- lingo-vocabulary/src/main/java/com/moncoder/lingo/vocabulary/LingoVocabularyApplication.java
+++ lingo-vocabulary/src/main/java/com/moncoder/lingo/vocabulary/LingoVocabularyApplication.java
@@ -1,9 +1,11 @@
 package com.moncoder.lingo.vocabulary;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableAdminServer
 @EnableTransactionManagement
 @SpringBootApplication(scanBasePackages = {"com.moncoder.lingo"})
 public class LingoVocabularyApplication {
